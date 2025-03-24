87144423bc2bea0b6ee4e11923025f60908b46c1
KKaan26 <kaannkocaali26@gmail.com>
2025-03-24 13:23:02 +0300

    ilk yükleme
    ...

---
+++ ProjectManager/Controllers/ProjectController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+public class ProjectsController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public ProjectsController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        return View(await _context.Projects.ToListAsync());
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Project project)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(project);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(project);
+    }
+
+    public async Task<IActionResult> Edit(int id)
+    {
+        var project = await _context.Projects.FindAsync(id);
+        if (project == null) return NotFound();
+        return View(project);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(int id, Project project)
+    {
+        if (id != project.Id) return NotFound();
+
+        if (ModelState.IsValid)
+        {
+            _context.Update(project);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(project);
+    }
+
+    public async Task<IActionResult> Delete(int id)
+    {
+        var project = await _context.Projects.FindAsync(id);
+        if (project == null) return NotFound();
+        return View(project);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var project = await _context.Projects.FindAsync(id);
+        _context.Projects.Remove(project);
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+}
---
+++ ProjectManager/Models/Project.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Project
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    [DataType(DataType.Date)]
+    public DateTime StartDate { get; set; }
+
+    [DataType(DataType.Date)]
+    public DateTime EstimatedEndDate { get; set; }
+}
